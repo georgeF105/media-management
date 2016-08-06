c98794d48cebb4b6be7574bf32e0b62dfd2a01dd
george francis <georgefrancis105@gmail.com>
2016-08-06 18:04:20 +1200

    Make move-to-pending.sh

---
+++ scripts/move-to-pending.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# moves given file to 'to be sorted file'
+
+# Input Parameters
+ARG_PATH="$3"
+ARG_NAME="$2"
+ARG_LABEL="N/A"
+
+CONFIG_OUTPUT="/mnt/2630A42C1E187F93/To be sorted"
+
+echo >> 'ARG_PATH:' + ARG_PATH '  ARG_NAME:' + ARG_NAME + '  ARG_LABEL:' + ARG_LABEL
