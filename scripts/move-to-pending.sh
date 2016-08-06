dd8b549254a61c475c03bd619f51f7959d886038
George Francis <georgefrancis105@gmail.com>
2016-08-06 22:21:57 +1200

    Fix Move-to-pending.sh script

--- scripts/move-to-pending.sh
+++ scripts/move-to-pending.sh
@@ -7,12 +7,13 @@ ARG_PATH="$3"
 ARG_NAME="$2"
 ARG_LABEL="N/A"
 
-CONFIG_OUTPUT="/mnt/2630A42C1E187F93/To be sorted/"
+CONFIG_OUTPUT="/mnt/2630A42C1E187F93/To be sorted"
 
 output='ARG_PATH:'$ARG_PATH'  ARG_NAME:'$ARG_NAME'  ARG_LABEL:'$ARG_LABEL
 
 echo "test output here:"$output
 echo $output >> /tmp/.media-man.log
-echo "input: "$ARG_PATH"/"$ARG_NAME "  output:"$CONFIG_OUTPUT"/"$ARG_NAME >> /tmp/.media-man.log
+INPUT=$ARG_PATH"/"$ARG_NAME
+OUTPUT=$CONFIG_OUTPUT
 
-cp $ARG_PATH"/"$ARG_NAME $CONFIG_OUTPUT"/"$ARG_NAME
+cp -R "$INPUT" "$OUTPUT"
