e0dce3f630f311ee34b6c2742fba7ba0a9bcb4b2
George Francis <georgefrancis105@gmail.com>
2016-08-08 10:42:21 +1200

    Do not run on hidden files

--- scripts/process-files.sh
+++ scripts/process-files.sh
@@ -6,14 +6,15 @@ sortPath = "/home/george/media_dump"
 sortPath2 = "/mnt/2630A42C1E187F93/To be sorted"
 
 
-for file in os.listdir(sortPath):
-	print "Name = " + file
-	subprocess.call(['./filebot-Rename.sh','0', file, sortPath])
-	# subprocess.call("./deluge-test.sh")
+#for file in os.listdir(sortPath):
+#	print "Name = " + file
+#	subprocess.call(['./filebotr-rename.sh','0', file, sortPath])
+#	# subprocess.call("./deluge-test.sh")
 
 for file in os.listdir(sortPath2):
 	print "Name = " + file
-	subprocess.call(['./filebot-Rename.sh','0', file, sortPath2])
+	if not file.startswith('.'):
+		subprocess.call(['./filebot-rename.sh','0', file, sortPath2])
 	# subprocess.call("./deluge-test.sh")
 
 
