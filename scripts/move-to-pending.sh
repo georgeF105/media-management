5c1581b256474794b7163897d536eb218ac0a062
george francis <georgefrancis105@gmail.com>
2016-09-03 18:32:57 +1200

    Add filebot script to move-to-pending

--- scripts/move-to-pending.sh
+++ scripts/move-to-pending.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-# moves given file to 'to be sorted file'
+# moves given file to 'to be sorted file' and then runs filebot rename script on it
 
 # Input Parameters
 ARG_PATH="$3"
 ARG_NAME="$2"
 ARG_LABEL="N/A"
 
-CONFIG_OUTPUT="/mnt/2630A42C1E187F93/To be sorted"
+PENDING_PATH="/mnt/2630A42C1E187F93/To be sorted"
+MEDIA_PATH="/mnt/2630A42C1E187F93"
 
 output='ARG_PATH:'$ARG_PATH'  ARG_NAME:'$ARG_NAME'  ARG_LABEL:'$ARG_LABEL
 
-echo "test output here:"$output
 echo $output >> /tmp/.media-man.log
 INPUT=$ARG_PATH"/"$ARG_NAME
-OUTPUT=$CONFIG_OUTPUT
+OUTPUT=$PENDING_PATH
 
 cp -R "$INPUT" "$OUTPUT"
+
+filebot -script fn:amc --output "$MEDIA_PATH" --action move --conflict skip -non-strict --log-file /tmp/.media-man.log --def plex=localhost:32400 unsorted=y music=y artwork=y excludeList=".excludes" ut_dir="$PENDING_PATH" ut_kind="multi" ut_title="$ARG_NAME" ut_label="$ARG_LABEL"
